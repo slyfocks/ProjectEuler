026cd595f833b3ebe2bf5da0eca7a72dabe8926f
slyfocks <imp@gliazzo.com>
2013-08-14 23:28:18 -0500

    fixed grid traversal

--- 015/015.py
+++ 015/015.py
@@ -1,13 +1,14 @@
 __author__ = 'slyfocks'
 import numpy as np
 
+
 #gives number of ways to traverse an n x m grid
 def lattice_paths(n, m):
     #initialize matrix
-    lattice = [np.zeros(n) for y in range(m)]
+    lattice = np.zeros((n+2, m+2))
     lattice[0][0], lattice[0][1], lattice[1][0] = 1, 1, 1
-    for x in range(1, n):
-        for y in range(1, m):
+    for x in range(1, n+2):
+        for y in range(1, m+2):
             lattice[x][y] = lattice[x-1][y] + lattice[x][y-1]
-    return lattice[n-1][m-1]
+    return lattice[n+1][m]
 print(lattice_paths(20, 20))
